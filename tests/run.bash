5485fe52d439a5b90f2f33977271b472d4d00684
jeromeshhh <156536587+jeromeshhh@users.noreply.github.com>
2025-10-18 04:59:34 +0800

    Update run.bash

--- tests/run.bash
+++ tests/run.bash
@@ -2,6 +2,6 @@
 
 # Copyright 2015 basebuilder authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
-# license that can be found in the LICENSE file.
+# license that can be found in the LICENSE file,
 
 sudo -E -iu $SUDO_USER /vagrant/platforms.bash "$1"
